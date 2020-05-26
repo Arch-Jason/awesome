00a5adae549fb297bf0ae6425e6f6c594de37471
Arch-Jason <arch-jason@outlook.com>
2020-05-26 14:23:20 +0800

    first commit

---
+++ autostart.sh
@@ -0,0 +1,10 @@
+
+variety &
+bash ~/.config/awesome/setwallpaper.sh
+setxkbmap us colemak
+ibus-daemon
+compton -b
+xset dpms 300 300
+nm-applet &
+kmix &
+albert &
---
+++ setwallpaper.sh
@@ -0,0 +1,7 @@
+xrandr --output eDP --off --output HDMI-A-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off
+while true
+do
+    feh --bg-fill ~/Pictures/variety/variety-copied-wallpaper*
+    rm -rf ~/Pictures/variety/variety-copied-wallpaper*
+    sleep 5
+done
